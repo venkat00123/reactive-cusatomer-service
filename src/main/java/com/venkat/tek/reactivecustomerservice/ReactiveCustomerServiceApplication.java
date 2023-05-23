0d7244c03e8a902fffe3fc70f8a91ec508ea5b01
venkat <venkat.avvaru3@gmail.com>
2023-05-23 18:10:48 +0530

    initial commit

---
+++ src/main/java/com/venkat/tek/reactivecustomerservice/ReactiveCustomerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.venkat.tek.reactivecustomerservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/venkat/tek/reactivecustomerservice/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.venkat.tek.reactivecustomerservice;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReactiveCustomerServiceApplication.class);
+	}
+
+}
